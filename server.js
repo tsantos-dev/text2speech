6be9f74aa85192de33d86764bc77873f7fdb6302
tsantos-dev <tavolaag@gmail.com>
2025-04-16 22:51:08 -0400

    nova tentativa de resolver a rota

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use(bodyParser.json());
 // Servir arquivos estáticos da raiz (ajuste se seu HTML/CSS/JS estiver em outra pasta, ex: 'public')
 // app.use(express.static("."));
 // app.use(express.static(path.join(__dirname, ".")));
-
 app.use(express.static(staticDir, { index: "index.html" }));
 
 // Middleware para log de requisições
